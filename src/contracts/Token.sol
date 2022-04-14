52cdb91c8899c0a40955218808f46823000e9e33
karthik.kun009@gmail.com <kunkarthik@pc>
2022-04-14 11:03:33 +0530

    transfer token functionality

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
 	using SafeMath for uint;
@@ -13,4 +14,16 @@ contract Token {
 		totalSupply = 1000000 * (10 ** decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success){
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
+		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+		balanceOf[_to] = balanceOf[_to].add(_value);
+		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	event Transfer(address indexed from, address indexed to, uint256 value);
+
 }
